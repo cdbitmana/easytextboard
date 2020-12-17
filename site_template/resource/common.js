d9493d94380bf28d82a592179ef2cc2f47d9b730
cdbitmana <cdbitmana@gmail.com>
2020-12-17 15:36:34 +0900

    토스트UI 플러그인 적용

--- site_template/resource/common.js
+++ site_template/resource/common.js
@@ -2,6 +2,8 @@ function ArticleDetail__Body__init() {
 
     var body = document.querySelector('.article_body');
     var initialValue = body.innerHTML.trim();
+    var initialValue = initialValue.replace("&gt;",">");
+    var initialValue = initialValue.replace("&lt;","<");
 
     var viewer = new toastui.Editor.factory({
         
