991af0e2627008b8e4c48d29059aaefa01b9fca9
cdbitmana <cdbitmana@gmail.com>
2021-01-07 00:19:30 +0900

    js script 위치 수정

--- src/com/sbs/example/easytextboard/service/BuildService.java
+++ src/com/sbs/example/easytextboard/service/BuildService.java
@@ -633,8 +633,7 @@ public class BuildService {
 						board_list.append("</tr>");
 						board_list.append("<tr>");
 						board_list.append("<td class=\"cell-id\">" + "{{article.id}}" + "</td>");
-						board_list.append("<td class=\"cell-title\"><a :href=\"" + board.getCode() + "-detail-"
-								+ "{{article.id}}" + ".html\">" + "{{article.title}}" + "</td>");
+						board_list.append("<td class=\"cell-title\"><a :href=\"'" + board.getCode() + "-detail-'+article.id+'.html'\">" + "{{article.title}}" + "</td>");
 						board_list.append("<td class=\"cell-writer\">" + "{{article.writer}}" + "</td>");
 						board_list.append("<td class=\"cell-regDate\">" + "{{article.regDate}}" + "</td>");
 						board_list.append("<td class=\"cell-hit\"></td>");
